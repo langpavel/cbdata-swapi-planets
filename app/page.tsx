4eb1be1bcaa01f9143b07a6f35c70b34d5141527
Pavel Lang <langpavel@phpskelet.org>
2026-02-27 06:51:39 +0100

    fix: fix navigation, remove unsupported links

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { appNavigationList } from "@/components/appNavigationList";
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-4xl font-bold text-center mt-8">
         Star Wars API explorer
       </h1>
+
+      <nav className="mt-12 flex flex-col items-center gap-4">
+        {appNavigationList.map((item) => (
+          <a
+            key={item.label}
+            href={item.href}
+            className="text-xl hover:underline"
+          >
+            {item.label}
+          </a>
+        ))}
+      </nav>
     </main>
   );
 }
--- components/appNavigationList.ts
+++ components/appNavigationList.ts
@@ -1,8 +1,8 @@
 export const appNavigationList = [
   { label: "Planets", href: "/planets" },
-  { label: "People", href: "#people" },
-  { label: "Species", href: "#species" },
-  { label: "Starships", href: "#starships" },
-  { label: "Vehicles", href: "#vehicles" },
-  { label: "Films", href: "#films" },
+  // { label: "People", href: "/people" },
+  // { label: "Species", href: "/species" },
+  // { label: "Starships", href: "/starships" },
+  // { label: "Vehicles", href: "/vehicles" },
+  // { label: "Films", href: "/films" },
 ] as const;
