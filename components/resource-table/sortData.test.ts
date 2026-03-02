tabulky/swapi-react

feat(resource-table): add generic ResourceTable component

components/resource-table/sortData.test.ts
@@ -1,0 +1,198 @@
+import type { ColumnDef, SortEntry } from "./columns";
+import { sortData } from "./sortData";
+
+// ---------------------------------------------------------------------------
+// Shared test data
+// ---------------------------------------------------------------------------
+
+type TestRow = {
+  name: string;
+  height: number | string;
+  mass: number | string;
+  gender: string;
+};
+
+const columns: ColumnDef<TestRow>[] = [
+  {
+    id: "name",
+    header: "Name",
+    sortable: true,
+    getSortValue: (r) => r.name,
+    renderCell: (r) => r.name,
+  },
+  {
+    id: "height",
+    header: "Height",
+    sortable: true,
+    getSortValue: (r) => (typeof r.height === "number" ? r.height : null),
+    fullCell: true,
+    renderCell: () => null as never, // not used in sorting tests
+  },
+  {
+    id: "mass",
+    header: "Mass",
+    sortable: true,
+    getSortValue: (r) => (typeof r.mass === "number" ? r.mass : null),
+    fullCell: true,
+    renderCell: () => null as never,
+  },
+  {
+    id: "gender",
+    header: "Gender",
+    // intentionally NOT sortable — no getSortValue
+    renderCell: (r) => r.gender,
+  },
+];
+
+const data: TestRow[] = [
+  { name: "Luke", height: 172, mass: 77, gender: "male" },
+  { name: "Leia", height: 150, mass: 49, gender: "female" },
+  { name: "Vader", height: 202, mass: 136, gender: "male" },
+  { name: "Yoda", height: 66, mass: 17, gender: "male" },
+  { name: "R2-D2", height: 96, mass: 32, gender: "n/a" },
+];
+
+// ---------------------------------------------------------------------------
+// Tests
+// ---------------------------------------------------------------------------
+
+describe("sortData", () => {
+  it("returns a shallow copy when no sort entries are provided", () => {
+    const result = sortData(data, columns, []);
+    expect(result).toEqual(data);
+    expect(result).not.toBe(data); // new array
+  });
+
+  it("does not mutate the input array", () => {
+    const frozen = Object.freeze([...data]);
+    const entries: SortEntry[] = [{ columnId: "name", direction: "asc" }];
+    expect(() => sortData(frozen, columns, entries)).not.toThrow();
+  });
+
+  // ---------- Single column sort ----------
+
+  it("sorts ascending by string column", () => {
+    const result = sortData(data, columns, [
+      { columnId: "name", direction: "asc" },
+    ]);
+    expect(result.map((r) => r.name)).toEqual([
+      "Leia",
+      "Luke",
+      "R2-D2",
+      "Vader",
+      "Yoda",
+    ]);
+  });
+
+  it("sorts descending by string column", () => {
+    const result = sortData(data, columns, [
+      { columnId: "name", direction: "desc" },
+    ]);
+    expect(result.map((r) => r.name)).toEqual([
+      "Yoda",
+      "Vader",
+      "R2-D2",
+      "Luke",
+      "Leia",
+    ]);
+  });
+
+  it("sorts ascending by numeric column", () => {
+    const result = sortData(data, columns, [
+      { columnId: "height", direction: "asc" },
+    ]);
+    expect(result.map((r) => r.name)).toEqual([
+      "Yoda", // 66
+      "R2-D2", // 96
+      "Leia", // 150
+      "Luke", // 172
+      "Vader", // 202
+    ]);
+  });
+
+  it("sorts descending by numeric column", () => {
+    const result = sortData(data, columns, [
+      { columnId: "height", direction: "desc" },
+    ]);
+    expect(result.map((r) => r.name)).toEqual([
+      "Vader", // 202
+      "Luke", // 172
+      "Leia", // 150
+      "R2-D2", // 96
+      "Yoda", // 66
+    ]);
+  });
+
+  // ---------- Multi-column sort ----------
+
+  it("breaks ties with secondary sort column", () => {
+    // All males first by gender-alpha won't help (gender is not sortable),
+    // so build a scenario with actual ties:
+    const rows: TestRow[] = [
+      { name: "A", height: 100, mass: 50, gender: "male" },
+      { name: "B", height: 100, mass: 30, gender: "female" },
+      { name: "C", height: 100, mass: 80, gender: "male" },
+      { name: "D", height: 200, mass: 10, gender: "female" },
+    ];
+
+    const result = sortData(rows, columns, [
+      { columnId: "height", direction: "asc" },
+      { columnId: "mass", direction: "desc" },
+    ]);
+
+    // height=100 tie → mass desc: C(80), A(50), B(30)  |  height=200: D
+    expect(result.map((r) => r.name)).toEqual(["C", "A", "B", "D"]);
+  });
+
+  // ---------- Null handling ----------
+
+  it("pushes null sort values to the end regardless of direction", () => {
+    const rows: TestRow[] = [
+      { name: "Known", height: 100, mass: 50, gender: "m" },
+      { name: "Unknown", height: "unknown", mass: "n/a", gender: "m" },
+      { name: "Tall", height: 200, mass: 80, gender: "m" },
+    ];
+
+    // Ascending — nulls last
+    const asc = sortData(rows, columns, [
+      { columnId: "height", direction: "asc" },
+    ]);
+    expect(asc.map((r) => r.name)).toEqual(["Known", "Tall", "Unknown"]);
+
+    // Descending — nulls still last
+    const desc = sortData(rows, columns, [
+      { columnId: "height", direction: "desc" },
+    ]);
+    expect(desc.map((r) => r.name)).toEqual(["Tall", "Known", "Unknown"]);
+  });
+
+  // ---------- Edge cases ----------
+
+  it("silently skips unknown column IDs in sort entries", () => {
+    const result = sortData(data, columns, [
+      { columnId: "nonexistent", direction: "asc" },
+    ]);
+    expect(result).toEqual(data);
+  });
+
+  it("silently skips non-sortable columns in sort entries", () => {
+    const result = sortData(data, columns, [
+      { columnId: "gender", direction: "asc" },
+    ]);
+    expect(result).toEqual(data);
+  });
+
+  it("handles empty data array", () => {
+    const result = sortData([], columns, [
+      { columnId: "name", direction: "asc" },
+    ]);
+    expect(result).toEqual([]);
+  });
+
+  it("handles single-element array", () => {
+    const result = sortData([data[0]], columns, [
+      { columnId: "name", direction: "asc" },
+    ]);
+    expect(result).toEqual([data[0]]);
+  });
+});

components/resource-table/sortData.ts
@@ -1,0 +1,87 @@
+import type { ColumnDef, SortEntry, SortValue } from "./columns";
+
+/**
+ * Compare two `SortValue`s.
+ *
+ * Rules:
+ * - `null` always sorts **last** regardless of direction.
+ * - Numbers compare numerically.
+ * - Strings compare via `localeCompare` (case-insensitive).
+ * - Mixed number/string: numbers sort before strings.
+ *
+ * Returns negative / zero / positive like `Array.prototype.sort` expects.
+ */
+function compareSortValues(a: SortValue, b: SortValue): number {
+  // Both null → equal
+  if (a === null && b === null) return 0;
+  // Null always last (regardless of asc/desc — caller inverts for direction)
+  if (a === null) return 1;
+  if (b === null) return -1;
+
+  // Same type fast-paths
+  if (typeof a === "number" && typeof b === "number") return a - b;
+  if (typeof a === "string" && typeof b === "string")
+    return a.localeCompare(b, undefined, { sensitivity: "base" });
+
+  // Mixed: numbers before strings
+  if (typeof a === "number") return -1;
+  return 1;
+}
+
+/**
+ * Sort `data` by a multi-column sort specification.
+ *
+ * Columns are resolved by `id` from the provided `columns` array.
+ * Unknown column IDs or non-sortable columns in `sortEntries` are silently
+ * skipped (defensive against stale URL params).
+ *
+ * Returns a **new array** — the input is never mutated.
+ */
+export function sortData<T>(
+  data: readonly T[],
+  columns: readonly ColumnDef<T>[],
+  sortEntries: readonly SortEntry[],
+): T[] {
+  if (sortEntries.length === 0) return data.slice();
+
+  // Pre-resolve columns & accessors for the active sort entries
+  const resolvedEntries = sortEntries
+    .map((entry) => {
+      const col = columns.find((c) => c.id === entry.columnId);
+      if (!col || !col.sortable) return null;
+      return {
+        getSortValue: col.getSortValue,
+        direction: entry.direction,
+      };
+    })
+    .filter(
+      (
+        e,
+      ): e is {
+        getSortValue: (item: T) => SortValue;
+        direction: "asc" | "desc";
+      } => e !== null,
+    );
+
+  if (resolvedEntries.length === 0) return data.slice();
+
+  return data.slice().sort((a, b) => {
+    for (const { getSortValue, direction } of resolvedEntries) {
+      const va = getSortValue(a);
+      const vb = getSortValue(b);
+      let cmp = compareSortValues(va, vb);
+
+      // Null-last is absolute (not flipped by direction).
+      // Only flip non-null comparisons.
+      if (cmp !== 0) {
+        const aNull = va === null;
+        const bNull = vb === null;
+        if (!aNull && !bNull && direction === "desc") {
+          cmp = -cmp;
+        }
+        return cmp;
+      }
+    }
+    return 0;
+  });
+}

components/resource-table/useTableState.ts
@@ -1,0 +1,169 @@
+"use client";
+
+import { useCallback, useMemo, useState } from "react";
+
+import type { ColumnDef, SortEntry } from "./columns";
+import { sortData } from "./sortData";
+
+// ---------------------------------------------------------------------------
+// Public return type
+// ---------------------------------------------------------------------------
+
+export type ColumnVisibility<T> = {
+  readonly column: ColumnDef<T>;
+  readonly visible: boolean;
+};
+
+export type TableState<T> = {
+  /** Visible columns in their current display order. */
+  readonly visibleColumns: ColumnDef<T>[];
+  /** All columns with their visibility flag (for future settings UI). */
+  readonly allColumns: ColumnVisibility<T>[];
+  /** Active multi-column sort chain. */
+  readonly sortEntries: SortEntry[];
+  /** Sorted (or unsorted) data, or `null` when not yet loaded. */
+  readonly sortedData: T[] | null;
+
+  // — Mutators —
+
+  /** Toggle a column's visibility on/off. */
+  readonly toggleColumn: (columnId: string) => void;
+  /** Replace the full column order (array of visible IDs). Pass `null` to reset to default. */
+  readonly setColumnOrder: (orderedIds: string[] | null) => void;
+  /**
+   * Toggle sorting on a column.
+   *
+   * - `multi=false` (default): replaces entire sort → cycles asc → desc → none.
+   * - `multi=true`: adds/cycles/removes this column within the sort chain.
+   */
+  readonly toggleSort: (columnId: string, multi?: boolean) => void;
+  /** Remove all sort entries. */
+  readonly clearSort: () => void;
+};
+
+// ---------------------------------------------------------------------------
+// Hook
+// ---------------------------------------------------------------------------
+
+/**
+ * Manages column visibility, order, and sort state via `localStorage`.
+ *
+ * Uses `useSyncExternalStore` so the server snapshot returns `null`
+ * (= default columns, no sort), avoiding hydration mismatches.
+ */
+export function useTableState<T>(
+  columns: readonly ColumnDef<T>[],
+  data: T[] | null,
+): TableState<T> {
+  // ----- State -----
+
+  const [visibleIds, setVisibleIds] = useState<string[] | null>(null);
+  const [sortEntries, setSortEntries] = useState<SortEntry[]>([]);
+
+  // ----- Derive visible & all columns -----
+
+  const visibleColumns = useMemo<ColumnDef<T>[]>(() => {
+    if (visibleIds === null) return columns.slice() as ColumnDef<T>[];
+    // Resolve IDs back to column defs, preserving the stored ordering.
+    // Unknown IDs (stale values) are silently dropped.
+    const colMap = new Map(columns.map((c) => [c.id, c]));
+    return visibleIds
+      .map((id) => colMap.get(id))
+      .filter((c): c is ColumnDef<T> => c !== undefined);
+  }, [columns, visibleIds]);
+
+  const allColumns = useMemo<ColumnVisibility<T>[]>(() => {
+    const visibleSet = new Set(visibleColumns.map((c) => c.id));
+    return columns.map((col) => ({
+      column: col as ColumnDef<T>,
+      visible: visibleSet.has(col.id),
+    }));
+  }, [columns, visibleColumns]);
+
+  // ----- Sorted data -----
+
+  const sortedData = useMemo<T[] | null>(() => {
+    if (data === null) return null;
+    return sortData(data, columns as ColumnDef<T>[], sortEntries);
+  }, [data, columns, sortEntries]);
+
+  // ----- Mutators -----
+
+  const toggleColumn = useCallback(
+    (columnId: string) => {
+      // Current visible IDs (or all if defaulted)
+      const currentIds = visibleIds ?? (columns.map((c) => c.id) as string[]);
+      const idx = currentIds.indexOf(columnId);
+      const nextIds =
+        idx >= 0
+          ? currentIds.filter((id) => id !== columnId)
+          : [...currentIds, columnId];
+
+      // If result matches the default set & order, clear the stored value
+      const isDefault =
+        nextIds.length === columns.length &&
+        nextIds.every((id, i) => id === columns[i].id);
+
+      setVisibleIds(isDefault ? null : nextIds);
+    },
+    [visibleIds, columns],
+  );
+
+  const setColumnOrder = useCallback((orderedIds: string[] | null) => {
+    setVisibleIds(orderedIds);
+  }, []);
+
+  const toggleSort = useCallback(
+    (columnId: string, multi = false) => {
+      // Only allow toggling sortable columns
+      const col = columns.find((c) => c.id === columnId);
+      if (!col || !col.sortable) return;
+
+      const current = sortEntries;
+      let next: SortEntry[];
+
+      const existingIdx = current.findIndex((e) => e.columnId === columnId);
+
+      if (multi) {
+        // Multi-column: add / cycle / remove within chain
+        if (existingIdx === -1) {
+          next = [...current, { columnId, direction: "asc" }];
+        } else if (current[existingIdx].direction === "asc") {
+          next = current.map((e, i) =>
+            i === existingIdx ? { ...e, direction: "desc" as const } : e,
+          );
+        } else {
+          // Was desc → remove
+          next = current.filter((_, i) => i !== existingIdx);
+        }
+      } else {
+        // Single-column: replace entire sort
+        if (existingIdx === -1) {
+          next = [{ columnId, direction: "asc" }];
+        } else if (current[existingIdx].direction === "asc") {
+          next = [{ columnId, direction: "desc" }];
+        } else {
+          next = []; // Was desc → clear
+        }
+      }
+
+      setSortEntries(next);
+    },
+    [columns, sortEntries],
+  );
+
+  const clearSort = useCallback(() => {
+    setSortEntries([]);
+  }, []);
+
+  return {
+    visibleColumns,
+    allColumns,
+    sortEntries,
+    sortedData,
+    toggleColumn,
+    setColumnOrder,
+    toggleSort,
+    clearSort,
+  };
+}

langpavel/cbdata-swapi-planets

components/resource-table/sortData.test.ts
@@ -1,0 +1,196 @@
+import type { ColumnDef, SortEntry } from "./types";
+import { sortData } from "./sortData";
+
+// ---------------------------------------------------------------------------
+// Shared test data
+// ---------------------------------------------------------------------------
+
+type TestRow = {
+  name: string;
+  height: number | string;
+  mass: number | string;
+  gender: string;
+};
+
+const columns: ColumnDef<TestRow>[] = [
+  {
+    id: "name",
+    header: "Name",
+    getValue: (r) => r.name,
+    isSortable: true,
+    getSortValue: (r) => r.name,
+  },
+  {
+    id: "height",
+    header: "Height",
+    getValue: (r) => r.height,
+    isSortable: true,
+    getSortValue: (r) => (typeof r.height === "number" ? r.height : null),
+  },
+  {
+    id: "mass",
+    header: "Mass",
+    getValue: (r) => r.mass,
+    isSortable: true,
+    getSortValue: (r) => (typeof r.mass === "number" ? r.mass : null),
+  },
+  {
+    id: "gender",
+    header: "Gender",
+    getValue: (r) => r.gender,
+    // intentionally NOT sortable — no getSortValue
+  },
+];
+
+const data: TestRow[] = [
+  { name: "Luke", height: 172, mass: 77, gender: "male" },
+  { name: "Leia", height: 150, mass: 49, gender: "female" },
+  { name: "Vader", height: 202, mass: 136, gender: "male" },
+  { name: "Yoda", height: 66, mass: 17, gender: "male" },
+  { name: "R2-D2", height: 96, mass: 32, gender: "n/a" },
+];
+
+// ---------------------------------------------------------------------------
+// Tests
+// ---------------------------------------------------------------------------
+
+describe("sortData", () => {
+  it("returns a shallow copy when no sort entries are provided", () => {
+    const result = sortData(data, columns, []);
+    expect(result).toEqual(data);
+    expect(result).not.toBe(data); // new array
+  });
+
+  it("does not mutate the input array", () => {
+    const frozen = Object.freeze([...data]);
+    const entries: SortEntry[] = [{ columnId: "name", direction: "asc" }];
+    expect(() => sortData(frozen, columns, entries)).not.toThrow();
+  });
+
+  // ---------- Single column sort ----------
+
+  it("sorts ascending by string column", () => {
+    const result = sortData(data, columns, [
+      { columnId: "name", direction: "asc" },
+    ]);
+    expect(result.map((r) => r.name)).toEqual([
+      "Leia",
+      "Luke",
+      "R2-D2",
+      "Vader",
+      "Yoda",
+    ]);
+  });
+
+  it("sorts descending by string column", () => {
+    const result = sortData(data, columns, [
+      { columnId: "name", direction: "desc" },
+    ]);
+    expect(result.map((r) => r.name)).toEqual([
+      "Yoda",
+      "Vader",
+      "R2-D2",
+      "Luke",
+      "Leia",
+    ]);
+  });
+
+  it("sorts ascending by numeric column", () => {
+    const result = sortData(data, columns, [
+      { columnId: "height", direction: "asc" },
+    ]);
+    expect(result.map((r) => r.name)).toEqual([
+      "Yoda", // 66
+      "R2-D2", // 96
+      "Leia", // 150
+      "Luke", // 172
+      "Vader", // 202
+    ]);
+  });
+
+  it("sorts descending by numeric column", () => {
+    const result = sortData(data, columns, [
+      { columnId: "height", direction: "desc" },
+    ]);
+    expect(result.map((r) => r.name)).toEqual([
+      "Vader", // 202
+      "Luke", // 172
+      "Leia", // 150
+      "R2-D2", // 96
+      "Yoda", // 66
+    ]);
+  });
+
+  // ---------- Multi-column sort ----------
+
+  it("breaks ties with secondary sort column", () => {
+    // All males first by gender-alpha won't help (gender is not sortable),
+    // so build a scenario with actual ties:
+    const rows: TestRow[] = [
+      { name: "A", height: 100, mass: 50, gender: "male" },
+      { name: "B", height: 100, mass: 30, gender: "female" },
+      { name: "C", height: 100, mass: 80, gender: "male" },
+      { name: "D", height: 200, mass: 10, gender: "female" },
+    ];
+
+    const result = sortData(rows, columns, [
+      { columnId: "height", direction: "asc" },
+      { columnId: "mass", direction: "desc" },
+    ]);
+
+    // height=100 tie → mass desc: C(80), A(50), B(30)  |  height=200: D
+    expect(result.map((r) => r.name)).toEqual(["C", "A", "B", "D"]);
+  });
+
+  // ---------- Null handling ----------
+
+  it("pushes null sort values to the end regardless of direction", () => {
+    const rows: TestRow[] = [
+      { name: "Known", height: 100, mass: 50, gender: "m" },
+      { name: "Unknown", height: "unknown", mass: "n/a", gender: "m" },
+      { name: "Tall", height: 200, mass: 80, gender: "m" },
+    ];
+
+    // Ascending — nulls last
+    const asc = sortData(rows, columns, [
+      { columnId: "height", direction: "asc" },
+    ]);
+    expect(asc.map((r) => r.name)).toEqual(["Known", "Tall", "Unknown"]);
+
+    // Descending — nulls still last
+    const desc = sortData(rows, columns, [
+      { columnId: "height", direction: "desc" },
+    ]);
+    expect(desc.map((r) => r.name)).toEqual(["Tall", "Known", "Unknown"]);
+  });
+
+  // ---------- Edge cases ----------
+
+  it("silently skips unknown column IDs in sort entries", () => {
+    const result = sortData(data, columns, [
+      { columnId: "nonexistent", direction: "asc" },
+    ]);
+    expect(result).toEqual(data);
+  });
+
+  it("silently skips non-sortable columns in sort entries", () => {
+    const result = sortData(data, columns, [
+      { columnId: "gender", direction: "asc" },
+    ]);
+    expect(result).toEqual(data);
+  });
+
+  it("handles empty data array", () => {
+    const result = sortData([], columns, [
+      { columnId: "name", direction: "asc" },
+    ]);
+    expect(result).toEqual([]);
+  });
+
+  it("handles single-element array", () => {
+    const result = sortData([data[0]], columns, [
+      { columnId: "name", direction: "asc" },
+    ]);
+    expect(result).toEqual([data[0]]);
+  });
+});

components/resource-table/sortData.ts
@@ -1,0 +1,87 @@
+import type { ColumnDef, SortEntry, SortValue } from "./types";
+
+/**
+ * Compare two `SortValue`s.
+ *
+ * Rules:
+ * - `null` always sorts **last** regardless of direction.
+ * - Numbers compare numerically.
+ * - Strings compare via `localeCompare` (case-insensitive).
+ * - Mixed number/string: numbers sort before strings.
+ *
+ * Returns negative / zero / positive like `Array.prototype.sort` expects.
+ */
+function compareSortValues(a: SortValue, b: SortValue): number {
+  // Both null → equal
+  if (a === null && b === null) return 0;
+  // Null always last (regardless of asc/desc — caller inverts for direction)
+  if (a === null) return 1;
+  if (b === null) return -1;
+
+  // Same type fast-paths
+  if (typeof a === "number" && typeof b === "number") return a - b;
+  if (typeof a === "string" && typeof b === "string")
+    return a.localeCompare(b, undefined, { sensitivity: "base" });
+
+  // Mixed: numbers before strings
+  if (typeof a === "number") return -1;
+  return 1;
+}
+
+/**
+ * Sort `data` by a multi-column sort specification.
+ *
+ * Columns are resolved by `id` from the provided `columns` array.
+ * Unknown column IDs or non-sortable columns in `sortEntries` are silently
+ * skipped (defensive against stale URL params).
+ *
+ * Returns a **new array** — the input is never mutated.
+ */
+export function sortData<T extends Record<string, unknown>>(
+  data: readonly T[],
+  columns: readonly ColumnDef<T>[],
+  sortEntries: readonly SortEntry[],
+): T[] {
+  if (sortEntries.length === 0) return data.slice();
+
+  // Pre-resolve columns & accessors for the active sort entries
+  const resolvedEntries = sortEntries
+    .map((entry) => {
+      const col = columns.find((c) => c.id === entry.columnId);
+      if (!col || !col.isSortable) return null;
+      return {
+        getSortValue: col.getSortValue,
+        direction: entry.direction,
+      };
+    })
+    .filter(
+      (
+        e,
+      ): e is {
+        getSortValue: (item: T) => SortValue;
+        direction: "asc" | "desc";
+      } => e !== null,
+    );
+
+  if (resolvedEntries.length === 0) return data.slice();
+
+  return data.slice().sort((a, b) => {
+    for (const { getSortValue, direction } of resolvedEntries) {
+      const va = getSortValue(a);
+      const vb = getSortValue(b);
+      let cmp = compareSortValues(va, vb);
+
+      // Null-last is absolute (not flipped by direction).
+      // Only flip non-null comparisons.
+      if (cmp !== 0) {
+        const aNull = va === null;
+        const bNull = vb === null;
+        if (!aNull && !bNull && direction === "desc") {
+          cmp = -cmp;
+        }
+        return cmp;
+      }
+    }
+    return 0;
+  });
+}

components/resource-table/useTableState.ts
@@ -1,0 +1,169 @@
+"use client";
+
+import { useCallback, useMemo, useState } from "react";
+
+import type { ColumnDef, SortEntry } from "./types";
+import { sortData } from "./sortData";
+
+// ---------------------------------------------------------------------------
+// Public return type
+// ---------------------------------------------------------------------------
+
+export type ColumnVisibility<T extends Record<string, unknown>> = {
+  readonly column: ColumnDef<T>;
+  readonly visible: boolean;
+};
+
+export type TableState<T extends Record<string, unknown>> = {
+  /** Visible columns in their current display order. */
+  readonly visibleColumns: ColumnDef<T>[];
+  /** All columns with their visibility flag (for future settings UI). */
+  readonly allColumns: ColumnVisibility<T>[];
+  /** Active multi-column sort chain. */
+  readonly sortEntries: SortEntry[];
+  /** Sorted (or unsorted) data, or `null` when not yet loaded. */
+  readonly sortedData: T[] | null;
+
+  // — Mutators —
+
+  /** Toggle a column's visibility on/off. */
+  readonly toggleColumn: (columnId: string) => void;
+  /** Replace the full column order (array of visible IDs). Pass `null` to reset to default. */
+  readonly setColumnOrder: (orderedIds: string[] | null) => void;
+  /**
+   * Toggle sorting on a column.
+   *
+   * - `multi=false` (default): replaces entire sort → cycles asc → desc → none.
+   * - `multi=true`: adds/cycles/removes this column within the sort chain.
+   */
+  readonly toggleSort: (columnId: string, multi?: boolean) => void;
+  /** Remove all sort entries. */
+  readonly clearSort: () => void;
+};
+
+// ---------------------------------------------------------------------------
+// Hook
+// ---------------------------------------------------------------------------
+
+/**
+ * Manages column visibility, order, and sort state via `localStorage`.
+ *
+ * Uses `useSyncExternalStore` so the server snapshot returns `null`
+ * (= default columns, no sort), avoiding hydration mismatches.
+ */
+export function useTableState<T extends Record<string, unknown>>(
+  columns: readonly ColumnDef<T>[],
+  data: T[] | null,
+): TableState<T> {
+  // ----- State -----
+
+  const [visibleIds, setVisibleIds] = useState<string[] | null>(null);
+  const [sortEntries, setSortEntries] = useState<SortEntry[]>([]);
+
+  // ----- Derive visible & all columns -----
+
+  const visibleColumns = useMemo<ColumnDef<T>[]>(() => {
+    if (visibleIds === null) return columns.slice() as ColumnDef<T>[];
+    // Resolve IDs back to column defs, preserving the stored ordering.
+    // Unknown IDs (stale values) are silently dropped.
+    const colMap = new Map(columns.map((c) => [c.id, c]));
+    return visibleIds
+      .map((id) => colMap.get(id))
+      .filter((c): c is ColumnDef<T> => c !== undefined);
+  }, [columns, visibleIds]);
+
+  const allColumns = useMemo<ColumnVisibility<T>[]>(() => {
+    const visibleSet = new Set(visibleColumns.map((c) => c.id));
+    return columns.map((col) => ({
+      column: col as ColumnDef<T>,
+      visible: visibleSet.has(col.id),
+    }));
+  }, [columns, visibleColumns]);
+
+  // ----- Sorted data -----
+
+  const sortedData = useMemo<T[] | null>(() => {
+    if (data === null) return null;
+    return sortData(data, columns as ColumnDef<T>[], sortEntries);
+  }, [data, columns, sortEntries]);
+
+  // ----- Mutators -----
+
+  const toggleColumn = useCallback(
+    (columnId: string) => {
+      // Current visible IDs (or all if defaulted)
+      const currentIds = visibleIds ?? (columns.map((c) => c.id) as string[]);
+      const idx = currentIds.indexOf(columnId);
+      const nextIds =
+        idx >= 0
+          ? currentIds.filter((id) => id !== columnId)
+          : [...currentIds, columnId];
+
+      // If result matches the default set & order, clear the stored value
+      const isDefault =
+        nextIds.length === columns.length &&
+        nextIds.every((id, i) => id === columns[i].id);
+
+      setVisibleIds(isDefault ? null : nextIds);
+    },
+    [visibleIds, columns],
+  );
+
+  const setColumnOrder = useCallback((orderedIds: string[] | null) => {
+    setVisibleIds(orderedIds);
+  }, []);
+
+  const toggleSort = useCallback(
+    (columnId: string, multi = false) => {
+      // Only allow toggling sortable columns
+      const col = columns.find((c) => c.id === columnId);
+      if (!col || !col.isSortable) return;
+
+      const current = sortEntries;
+      let next: SortEntry[];
+
+      const existingIdx = current.findIndex((e) => e.columnId === columnId);
+
+      if (multi) {
+        // Multi-column: add / cycle / remove within chain
+        if (existingIdx === -1) {
+          next = [...current, { columnId, direction: "asc" }];
+        } else if (current[existingIdx].direction === "asc") {
+          next = current.map((e, i) =>
+            i === existingIdx ? { ...e, direction: "desc" as const } : e,
+          );
+        } else {
+          // Was desc → remove
+          next = current.filter((_, i) => i !== existingIdx);
+        }
+      } else {
+        // Single-column: replace entire sort
+        if (existingIdx === -1) {
+          next = [{ columnId, direction: "asc" }];
+        } else if (current[existingIdx].direction === "asc") {
+          next = [{ columnId, direction: "desc" }];
+        } else {
+          next = []; // Was desc → clear
+        }
+      }
+
+      setSortEntries(next);
+    },
+    [columns, sortEntries],
+  );
+
+  const clearSort = useCallback(() => {
+    setSortEntries([]);
+  }, []);
+
+  return {
+    visibleColumns,
+    allColumns,
+    sortEntries,
+    sortedData,
+    toggleColumn,
+    setColumnOrder,
+    toggleSort,
+    clearSort,
+  };
+}